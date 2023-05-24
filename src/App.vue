0fa1c23437b9142ab128fad96486415f9587898a
Eduardo da Silva <eduardo.silva@gmail.com>
2023-05-24 10:08:29 -0300

    Add basic listing

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+</script>
+
+<template>
+  <h1>Minha livraria</h1>
+  <div class="listagem-livros">
+    <div class="card-livro" v-for="livro in livros" :key="livro.id">
+      <div class="wrap-livro">
+        <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
+      </div>
+      <p class="titulo-livro">{{ livro.title }}</p>
+      <p class="autor-livro">{{ livro.author }}</p>
+      <p class="preco-livro">{{ livro.price }}</p>
+    </div>
+  </div>
+  <!-- {{ livros }} -->
+</template>
+
+<style scoped>
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card-livro {
+  margin: 5px 10px;
+  padding: 10px;
+  background-color: beige;
+  border-radius: 10px;
+  width: 180px;
+}
+
+.wrap-livro {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 10px;
+  width: 180px;
+  height: 270px;
+}
+.capa-livro {
+  width: 90%;
+  max-height: 100%;
+}
+
+.card-livro p {
+  margin: 0;
+}
+
+.card-livro .titulo-livro {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,44 @@
+export const livros = [
+  {
+    id: 1,
+    title: 'O Poderoso Chefão',
+    author: 'Mario Puzo',
+    price: 49.9,
+    img: 'https://m.media-amazon.com/images/I/31jGFI5wX2L._SX331_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 2,
+    title: 'O Senhor dos Anéis - Box completo',
+    author: 'J. R. R. Tolkien',
+    price: 129.9,
+    img: 'https://m.media-amazon.com/images/I/715afDdgKfL.jpg'
+  },
+  {
+    id: 3,
+    title: 'O Pequeno Príncipe',
+    author: 'Antoine de Saint-Exupéry',
+    price: 19.9,
+    img: 'https://m.media-amazon.com/images/I/41afCn3PQUL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
+  },
+  {
+    id: 4,
+    title: 'Dom Quixote',
+    author: 'Miguel de Cervantes',
+    price: 39.9,
+    img: 'https://m.media-amazon.com/images/I/513uHO8IDlL._SX359_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 5,
+    title: 'O Conde de Monte Cristo',
+    author: 'Alexandre Dumas',
+    price: 69.9,
+    img: 'https://m.media-amazon.com/images/I/81ZswN9PVPL._AC_UL320_.jpg'
+  },
+  {
+    id: 6,
+    title: 'O Hobbit',
+    author: 'J. R. R. Tolkien',
+    price: 39.9,
+    img: 'https://m.media-amazon.com/images/I/91M9xPIf10L._AC_UL320_.jpg'
+  }
+]
